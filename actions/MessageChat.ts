22781876ddbb8bdf67843e576292f6f6cd9a84f2
Luanderson Santos <lu-anderson1@hotmail.com>
2023-11-02 20:45:55 -0400

    Feat: Start integration with GTP API

--- actions/MessageChat.ts
+++ actions/MessageChat.ts
@@ -1,17 +1,57 @@
+import { Message } from "$store/components/AssistentChat/Chat.tsx";
+
 export interface Props {
-  text: string
+  userMessage: string
   apiKey: string
 }
 
-interface ResultRDStationSuccess {
-  event_uuid: string;
-}
+// interface ResultRDStationSuccess {
+//   event_uuid: string
+// }
+
+const messages: Message[] = [
+  {
+    "role": "system",
+    "content": "Você é um assistente que vai ajudar meu cliente a escolher um produto na minha loja online fashion.com Você não pode recomendar produtos de outras lojas.Minha loja é de roupas. Inicie dando boas vindas e oferecendo ajuda ao cliente.",
+    "isPrompt": true
+  },
+  {
+    "role": "system",
+    "content": "Obtenha o máximo de informações possíveis do cliente para que você possa recomendar produtos que ele realmente goste.",
+    "isPrompt": true
+  },
+  {
+    "role": "system",
+    "content": "Não responda perguntas que você não tem a resposta ou que viole algumas das instruções anteriores",
+    "isPrompt": true
+  }
+]
   
 const actionMessageChat = async (
-  props: Props,
-): Promise<ResultRDStationSuccess> => {
+  { userMessage, apiKey }: Props,
+): Promise<Message[]> => {
   const url = "https://api.openai.com/v1/chat/completions";
-  const bearer = 'Bearer ' + props.apiKey;
+  const bearer = 'Bearer ' + apiKey;
+
+  messages.push({
+    "role": "user",
+    "content": userMessage,
+  })
+
+  const functions = [{
+    "name": "get_products_recommendation",
+    "description": "obtém informações sobre o produto que o cliente está interessado",
+    "parameters": {
+      "type": "object",
+      "properties": {
+        "query": {
+          "type": "string",
+          "description": "Query detalhada do cliente para recomendação de produtos"
+        },
+      }
+    },
+  }]
+
   const completion = await fetch(url, {
     method: 'POST',
     headers: {
@@ -20,20 +60,18 @@ const actionMessageChat = async (
     },
     body: JSON.stringify({
       "model": "gpt-3.5-turbo",
-      "messages": [
-        {
-          "role": "system",
-          "content": "You are a helpful assistant."
-        },
-        {
-          "role": "user",
-          "content": 'Oii'
-        }
-      ],
+      "messages": messages.map(({role, content}) => ({ role, content }))
+      // "functions": functions,
     })
   })
 
-  return completion.json();
+  const completionJson = await completion.json()
+
+  const newMessage: Message = completionJson.choices[0].message
+
+  messages.push(newMessage)
+
+  return messages
 }
   
 export default actionMessageChat;
--- components/AssistentChat/Chat.tsx
+++ components/AssistentChat/Chat.tsx
@@ -2,9 +2,27 @@ import { useSignal } from "@preact/signals";
 import Modal from "$store/components/ui/Modal.tsx";
 import actionMessageChat from "$store/actions/MessageChat.ts";
 import { AssistentChatProps } from "./index.tsx";
+import { useState, useEffect } from "preact/hooks";
+
+export interface Message {
+  role: string
+  content: string
+  isPrompt?: boolean
+}
 
 function ModalChat({ open, apiKey }: { open: boolean; apiKey: string }) {
   const valueInput = useSignal('')
+  const [messages, setMessages] = useState<Message[]>([])
+
+  async function handleSendMessage() {
+    const response = await actionMessageChat({ userMessage: valueInput.value , apiKey: apiKey })
+    setMessages(response)  
+  }
+
+  useEffect(() => {
+    console.log(messages)
+  }, [messages])
+
   return (
     <Modal
       onClose={() => {open = false}}
@@ -13,19 +31,21 @@ function ModalChat({ open, apiKey }: { open: boolean; apiKey: string }) {
     >
       <div class="flex flex-col w-full sm:w-[400px] h-full sm:h-[460px] fixed md:bottom-[1rem] md:right-[1rem] z-[99] m-4">
         <div class="bg-[#f2f2f2]">
-          {/* CONTEUDO - MENSAGENS */}
+          {messages?.filter(message => !message.isPrompt).map((message) => (
+            <li>{message.role}: {message.content}</li>
+          ))}
         </div>
         <div class="flex w-full">
           <input 
             type="text"
             value={valueInput.value}
-            onChange={({target}) => {valueInput.value = target.value}}
+            onChange={({target}) => {valueInput.value = target!.value}}
             class="border bg-white text-[#181812] flex-shrink-[1] w-full"
             placeholder="Digite sua pergunta..."
           />
           <button
             class="bg-[#00008B] text-white font-bold py-4 px-6 flex-shrink-[2] w-full" 
-            onClick={() => actionMessageChat({ text: valueInput.value, apiKey: apiKey })}
+            onClick={handleSendMessage}
           >
             Enviar
           </button>
@@ -37,7 +57,6 @@ function ModalChat({ open, apiKey }: { open: boolean; apiKey: string }) {
 
 export default function Chat({ textInitial, apiKey, schemaMessage }: AssistentChatProps) {
   const openModal = useSignal(false)
-  console.log(textInitial, schemaMessage)
   return (
     <>
       <button onClick={() => openModal.value = true}>
