7b130bbef48e3e6f45d6a93090232e3d308b93a3
DanPedrosa35 <147006304+DanPedrosa35@users.noreply.github.com>
2023-11-03 01:45:18 +0000

    Feat: Adiciona algumas funções para o chat

--- components/AssistentChat/Chat.tsx
+++ components/AssistentChat/Chat.tsx
@@ -4,6 +4,11 @@ import actionMessageChat from "$store/actions/MessageChat.ts";
 import { AssistentChatProps } from "./index.tsx";
 import { useState, useEffect } from "preact/hooks";
 import Spinner from "deco-sites/truedevs/components/ui/Spinner.tsx";
+import { Suggestion } from "apps/commerce/types.ts";
+import { useSuggestions } from "$store/sdk/useSuggestions.ts";
+import { Resolved } from "deco/engine/core/resolver.ts";
+import { sendEvent } from "$store/sdk/analytics.tsx";
+import Icon from "deco-sites/truedevs/components/ui/Icon.tsx";
 
 export interface Message {
   role: string
@@ -11,10 +16,30 @@ export interface Message {
   isPrompt?: boolean
 }
 
-function ModalChat({ open, apiKey }: { open: boolean; apiKey: string }) {
+export interface ModalChatProps {
+  open: boolean
+  apiKey: string
+  loader: Resolved<Suggestion | null>;
+}
+
+function ModalChat({ open, apiKey, loader }: ModalChatProps) {
   const valueInput = useSignal('')
   const isLoading = useSignal(false)
   const [messages, setMessages] = useState<Message[]>([])
+  
+  const { setQuery, payload, loading } = useSuggestions(loader);
+  const { products = [], searches = [] } = payload.value ?? {};
+  const hasProducts = Boolean(products.length); {/* PRODUTOS ACHADOS */}
+  const hasTerms = Boolean(searches.length); {/* TERMOS SEMELHANTES */}
+  {/* FUNÇÃO QUE EXECUTA O INTELLISENSE SEARCH */}
+  {/* if (value) {
+    sendEvent({
+      name: "search",
+      params: { search_term: value },
+    });
+  }
+  
+  setQuery(value); */}
 
   async function handleSendMessage() {
     isLoading.value = true
@@ -34,7 +59,7 @@ function ModalChat({ open, apiKey }: { open: boolean; apiKey: string }) {
       open={open}
       class="justify-end items-end"
     >
-      <div class="flex flex-col w-full sm:w-[400px] h-full sm:h-[460px] fixed md:bottom-[1rem] md:right-[1rem] z-[99] m-4">
+      <div class="flex flex-col w-full sm:w-[400px] h-full sm:h-[460px] fixed md:bottom-[1rem] md:right-[1rem] z-[99] m-4 overflow-hidden rounded-2xl bg-[#f2f2f2]">
         <div class="bg-[#f2f2f2]">
           {messages?.filter(message => !message.isPrompt).map((message) => (
             <li>{message.role}: {message.content}</li>
@@ -60,17 +85,21 @@ function ModalChat({ open, apiKey }: { open: boolean; apiKey: string }) {
   )
 }
 
-export default function Chat({ textInitial, apiKey, schemaMessage }: AssistentChatProps) {
+export default function Chat({ textInitial, apiKey, schemaMessage, engineSuggestion }: AssistentChatProps) {
   const openModal = useSignal(false)
+
   return (
     <>
-      <button onClick={() => openModal.value = true}>
-        {/* <Icon /> */}
-        CHAT
+      <button
+        class="bg-[#00008B] text-white border-none rounded-full p-4"
+        onClick={() => openModal.value = true}
+      >
+        <Icon id="Message" size={40} />
       </button>
       <ModalChat
         apiKey={apiKey}
         open={openModal.value}
+        { ...engineSuggestion }
       />
     </>
   )
--- components/AssistentChat/index.tsx
+++ components/AssistentChat/index.tsx
@@ -1,19 +1,21 @@
 import Chat from "$store/components/AssistentChat/Chat.tsx";
+import type { Props as SearchbarProps } from "$store/components/search/Searchbar.tsx";
 
 export interface AssistentChatProps {
   textInitial: string;
   apiKey: string;
   schemaMessage: SchemaMessageEngine[]
+  engineSuggestion: SearchbarProps;
 }
 
 export interface SchemaMessageEngine {
   text: string
 }
 
-export default function AssistentChat({ textInitial, apiKey, schemaMessage }: AssistentChatProps) {
+export default function AssistentChat({ textInitial, apiKey, schemaMessage, engineSuggestion }: AssistentChatProps) {
   return (
-    <section class="fixed bottom-[2rem] right-[2rem]">
-      <Chat {...{ textInitial, apiKey, schemaMessage }} />
+    <section class="fixed bottom-[2rem] right-[2rem] z-50">
+      <Chat {...{ textInitial, apiKey, schemaMessage, engineSuggestion }} />
     </section>
   )
 }
